d4f43251ea71a52a33a9cd879fdc452d71cea50c
Ben Cresswell <bjcresswell@gmail.com>
2026-09-22 08:40:19 +1000

    Add code

---
+++ code/demo1.R
@@ -0,0 +1,11 @@
+# Demo script to load penguins data
+
+# Check wd
+getwd()
+
+# Broken code won't run (obviously)
+data("penguis")
+
+# Load penguins data
+data("penguins", package = "palmerpenguins")
+
